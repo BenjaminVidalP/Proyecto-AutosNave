bcdf2223c6ceb87c2db4aa41fc8c5b032bf1bc66
Angu017 <101602981+Angu017@users.noreply.github.com>
2022-06-22 11:53:17 -0400

    avance3

--- AutosNaves/api_rest/views.py
+++ AutosNaves/api_rest/views.py
@@ -11,26 +11,46 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 
-
-
 # Create your views here.
 
 
 @csrf_exempt
 # ----------------------Serializers1 ---------------
-@api_view(['GET', 'PUT', 'DELETE', 'POST'])
+def listado_Auto(request):
+    if request.method == 'GET':
+        Auto = Auto.objects.all()
+        serializer = AUTOSerializers(Auto, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AUTOSerializers(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
 @permission_classes((IsAuthenticated,))
-def modificarEliminarAUTO1(request, id):
+def agregar_AUTO(request):
+    data = JSONParser().parse(request)
+    serializer = AUTOSerializers(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+def modificarEliminarAUTO(request, id):
     try:
-        m = Auto.objects.get(idAuto=id)
+        m = Auto.objects.get(codigoChip=id)
     except Auto.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = AUTOSerializers(m)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
+    if request.method == 'PUT':
         data2 = JSONParser().parse(request)
         serializer = AUTOSerializers(m, data=data2)
         if serializer.is_valid():
@@ -43,42 +63,13 @@ def modificarEliminarAUTO1(request, id):
         m.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AUTOSerializers(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # ----------------------Serializers2 ---------------
-@api_view(['GET', 'PUT', 'DELETE', 'POST'])
-@permission_classes((IsAuthenticated,))
-def modificarEliminarAUTO2(request, id):
-    try:
-        m = Auto.objects.get(idAuto=id)
-    except Auto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+def listado_Auto2(request):
     if request.method == 'GET':
-        serializer = AUTOSerializers2(m)
+        Auto = Auto.objects.all()
+        serializer = AUTOSerializers2(Auto, many=True)
         return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        data2 = JSONParser().parse(request)
-        serializer = AUTOSerializers2(m, data=data2)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == "DELETE":
-        m.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = AUTOSerializers2(data=data)
@@ -88,24 +79,29 @@ def modificarEliminarAUTO2(request, id):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# ----------------------Serializers3 ---------------
 
-
-@api_view(['GET', 'PUT', 'DELETE', 'POST'])
+@api_view(['POST'])
 @permission_classes((IsAuthenticated,))
-def modificarEliminarAUTO3(request, id):
+def agregar_AUTO2(request):
+    data = JSONParser().parse(request)
+    serializer = AUTOSerializers2(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+def modificarEliminarAUTO2(request, id):
     try:
         m = Auto.objects.get(idAuto=id)
     except Auto.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = AUTOSerializers3(m)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
+    if request.method == 'PUT':
         data2 = JSONParser().parse(request)
-        serializer = AUTOSerializers3(m, data=data2)
+        serializer = AUTOSerializers(m, data=data2)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -116,11 +112,53 @@ def modificarEliminarAUTO3(request, id):
         m.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+# ----------------------Serializers3 ---------------
+
+
+def listado_Auto3(request):
+    if request.method == 'GET':
+        Auto = Auto.objects.all()
+        serializer = AUTOSerializers2(Auto, many=True)
+        return Response(serializer.data)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
-        serializer = AUTOSerializers3(data=data)
+        serializer = AUTOSerializers2(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def agregar_AUTO3(request):
+    data = JSONParser().parse(request)
+    serializer = AUTOSerializers2(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((IsAuthenticated,))
+def modificarEliminarAUTO3(request, id):
+    try:
+        m = Auto.objects.get(idAuto=id)
+    except Auto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        data2 = JSONParser().parse(request)
+        serializer = AUTOSerializers(m, data=data2)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        m.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
