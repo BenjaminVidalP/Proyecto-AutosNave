a3f5ad9032f256496346fea9cca6a43504800eaa
Angu017 <101602981+Angu017@users.noreply.github.com>
2022-06-21 22:25:38 -0400

    aaa

--- AutosNaves/api_rest/urls.py
+++ AutosNaves/api_rest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api_rest.views import modificarEliminarAUTO1, modificarEliminarAUTO2, modificarEliminarAUTO3
 from api_rest.viewsLogin import loginApi
 urlpatterns = [
-    path('modificarEliminarAUTO/<id>',modificarEliminarAUTO1,name="modificarEliminarAUTO"),
+    path('modificarEliminarAUTO1/<id>',modificarEliminarAUTO1,name="modificarEliminarAUTO1"),
     path('modificarEliminarAUTO2/<id>',modificarEliminarAUTO2,name="modificarEliminarAUTO2"),
     path('modificarEliminarAUTO3/<id>',modificarEliminarAUTO3,name="modificarEliminarAUTO3"),
     path('loginApi/',loginApi,name="loginApi"),
